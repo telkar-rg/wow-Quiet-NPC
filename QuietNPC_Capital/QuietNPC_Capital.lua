dd5cbd7547a86a53989ab42e071b199941427971
telkar-rg <telkar-rg@gmx.net>
2025-02-07 12:11:50 +0100

    added noisy vendors

--- QuietNPC_Capital/QuietNPC_Capital.lua
+++ QuietNPC_Capital/QuietNPC_Capital.lua
@@ -14,8 +14,127 @@ if Locale == "deDE" then
 	core:addMonsterName("Bürger der Verlassenen")
 	core:addMonsterName("Goblinbürger")
 	
+	-- Dalaran
 	core:addMonsterName("Jaeren Sonnenschwur") 	-- 35589
 	core:addMonsterName("Arelas Hellstern") 	-- 35589
+	core:addMonsterName("Hira Schneedämmerung") 	-- 31238
+	
+	core:addMonsterName("Inzi Zauberlicht") 	-- 28682
+	core:addMonsterName("Narisa Rotgold") 	-- 28685
+	core:addMonsterName("Caliel Hellweide") 	-- 28686
+	core:addMonsterName("Amisi Azurblick") 	-- 28687
+	core:addMonsterName("Susana Averoy") 	-- 28691
+	core:addMonsterName("\"Roter\" Jack Findel") 	-- 28692
+	core:addMonsterName("Jonas Timotheus") 	-- 28701
+	core:addMonsterName("Angelo Pescatore") 	-- 28707
+	core:addMonsterName("Ildine Sorgenspeer") 	-- 28714
+	core:addMonsterName("Endora Edelhaupt") 	-- 28715
+	core:addMonsterName("Palja Amboss") 	-- 28716
+	core:addMonsterName("Ranid Leuchtegold") 	-- 28718
+	core:addMonsterName("Tiffany Cartier") 	-- 28721
+	core:addMonsterName("Bryan Landers") 	-- 28722
+	core:addMonsterName("Larana Drome") 	-- 28723
+	core:addMonsterName("Patricia Egan") 	-- 28725
+	core:addMonsterName("Dominique Stefano") 	-- 28726
+	core:addMonsterName("Edward Egan") 	-- 28727
+	core:addMonsterName("Dorian Finnes") 	-- 28728
+	core:addMonsterName("Marcia Balzer") 	-- 28742
+	core:addMonsterName("Elizabeth Ross") 	-- 28776
+	core:addMonsterName("Breanni") 	-- 28951
+	core:addMonsterName("Aemara") 	-- 28989
+	core:addMonsterName("Anthony Durain") 	-- 28990
+	core:addMonsterName("Valaden Silberklinge") 	-- 28991
+	core:addMonsterName("Valerie Langrom") 	-- 28992
+	core:addMonsterName("Aerith Schlüsselblume") 	-- 28993
+	core:addMonsterName("Abra Kadabra") 	-- 28994
+	core:addMonsterName("Paldesse") 	-- 28995
+	core:addMonsterName("Griselda Hunderland") 	-- 28997
+	core:addMonsterName("Arille Azurblick") 	-- 29049
+	core:addMonsterName("Pakke Funkenleuchter") 	-- 29261
+	core:addMonsterName("Dagna Flintriegel") 	-- 29476
+	core:addMonsterName("Jepetto Spaßbrumm") 	-- 29478
+	core:addMonsterName("Karandonna") 	-- 29491
+	core:addMonsterName("Jarod Zieher") 	-- 29493
+	core:addMonsterName("Shen Kang Cheng") 	-- 29494
+	core:addMonsterName("Norvin Aldermann") 	-- 29495
+	core:addMonsterName("Kerta die Tapfere") 	-- 29496
+	core:addMonsterName("Walter Weißfurt") 	-- 29497
+	core:addMonsterName("Bartram Haller") 	-- 29499
+	core:addMonsterName("Linna Bruder") 	-- 29510
+	core:addMonsterName("Lalla Hellzwirn") 	-- 29511
+	core:addMonsterName("Ainderu Sommerblatt") 	-- 29512
+	core:addMonsterName("Bragund Hellweiß") 	-- 29523
+	core:addMonsterName("Orton Bennet") 	-- 29527
+	core:addMonsterName("Debbi Meer") 	-- 29528
+	core:addMonsterName("Ninsianna") 	-- 29529
+	core:addMonsterName("Ajay Grün") 	-- 29532
+	core:addMonsterName("Alchemist Cinesra") 	-- 29535
+	core:addMonsterName("Darahir") 	-- 29537
+	core:addMonsterName("Hexil Garrot") 	-- 29538
+	core:addMonsterName("Apfelzweig") 	-- 29547
+	core:addMonsterName("Aimee") 	-- 29548
+	core:addMonsterName("Angelique Butler") 	-- 29628
+	core:addMonsterName("Hagatha Edelhaupt") 	-- 29636
+	core:addMonsterName("Chameli Banaphash") 	-- 29702
+	core:addMonsterName("Gussi Glanzschein") 	-- 29703
+	core:addMonsterName("Lucian Trias") 	-- 29714
+	core:addMonsterName("Fialla Süßbeere") 	-- 29715
+	core:addMonsterName("Aufziehgehilfe") 	-- 29716
+	core:addMonsterName("Blazik Feuerklaue") 	-- 30885
+	core:addMonsterName("Misensi") 	-- 31031
+	core:addMonsterName("Derek Odds") 	-- 31032
+	core:addMonsterName("Hira Schneedämmerung") 	-- 31238
+	core:addMonsterName("Uda das Biest") 	-- 31557
+	core:addMonsterName("Arkanist Adurin") 	-- 31579
+	core:addMonsterName("Arkanistin Ivrenne") 	-- 31580
+	core:addMonsterName("Magister Brasael") 	-- 31581
+	core:addMonsterName("Magistrix Lambriesse") 	-- 31582
+	core:addMonsterName("Harold Winston") 	-- 32172
+	core:addMonsterName("Mei Francis") 	-- 32216
+	core:addMonsterName("Kyunghee") 	-- 32253
+	core:addMonsterName("Erzmagier Alvareaux") 	-- 32287
+	core:addMonsterName("Nixi Feuerklaue") 	-- 32334
+	core:addMonsterName("Christi Stockton") 	-- 32337
+	core:addMonsterName("Sandra Bartan") 	-- 32403
+	core:addMonsterName("Mato") 	-- 32412
+	core:addMonsterName("Isimari Sanftwind") 	-- 32413
+	core:addMonsterName("Hamaka") 	-- 32415
+	core:addMonsterName("Stefen Cotter") 	-- 32416
+	core:addMonsterName("Umbiwa") 	-- 32419
+	core:addMonsterName("Mimbihi") 	-- 32420
+	core:addMonsterName("Marcella Blüher") 	-- 32421
+	core:addMonsterName("Laire Braugold") 	-- 32424
+	core:addMonsterName("Coira Langflinte") 	-- 32426
+	core:addMonsterName("Brammold Tiefmine") 	-- 32509
+	core:addMonsterName("Vanessa Kaufmann") 	-- 32514
+	core:addMonsterName("Braeg Starkbart") 	-- 32515
+	core:addMonsterName("Alfred Copperworth") 	-- 32631
+	core:addMonsterName("Sarah Brady") 	-- 33026
+	core:addMonsterName("Jessica Kaufmann") 	-- 33027
+	core:addMonsterName("Nargel Peitschleine") 	-- 33936
+	core:addMonsterName("Xazi Schmauchpfeife") 	-- 33937
+	core:addMonsterName("Zom Bocom") 	-- 33938
+	core:addMonsterName("Magister Sarien") 	-- 33963
+	core:addMonsterName("Arkanist Firael") 	-- 33964
+	core:addMonsterName("Eisenfang Rüsti") 	-- 34095
+	core:addMonsterName("Dubin Lehm") 	-- 34252
+	core:addMonsterName("Arkanistin Miluria") 	-- 35494
+	core:addMonsterName("Magistrix Vesara") 	-- 35495
+	core:addMonsterName("Rueben Lauren") 	-- 35496
+	core:addMonsterName("Rafael Langrom") 	-- 35497
+	core:addMonsterName("Horace Hunderland") 	-- 35498
+	core:addMonsterName("Matilda Hellweiß") 	-- 35500
+	core:addMonsterName("Verzauberer Isian") 	-- 35507
+	core:addMonsterName("Verzauberer Erodin") 	-- 35508
+	core:addMonsterName("Usuri Münzglanz") 	-- 35790
+	core:addMonsterName("Kaye Toogie") 	-- 35826
+	core:addMonsterName("Magister Arlan") 	-- 37941
+	core:addMonsterName("Arkanist Uovril") 	-- 37942
+	core:addMonsterName("Frozo der Wohlbekannte") 	-- 40160
+
+	
+	-- Shattrath
+	core:addMonsterName("Cro Dickfädel") 	-- 19196
 
 else 	-- enUS enGB
 	core:addMonsterName("Human Commoner")
@@ -30,8 +149,127 @@ else 	-- enUS enGB
 	core:addMonsterName("Forsaken Commoner")
 	core:addMonsterName("Goblin Commoner")
 	
+	-- Dalaran
 	core:addMonsterName("Jaeren Sunsworn")
 	core:addMonsterName("Arelas Brightstar")
+	core:addMonsterName("Hira Snowdawn")
+	
+	core:addMonsterName("Inzi Charmlight")
+	core:addMonsterName("Narisa Redgold")
+	core:addMonsterName("Caliel Brightwillow")
+	core:addMonsterName("Amisi Azuregaze")
+	core:addMonsterName("Susana Averoy")
+	core:addMonsterName("\"Red\" Jack Findle")
+	core:addMonsterName("Timothy Jones")
+	core:addMonsterName("Angelo Pescatore")
+	core:addMonsterName("Ildine Sorrowspear")
+	core:addMonsterName("Endora Moorehead")
+	core:addMonsterName("Palja Amboss")
+	core:addMonsterName("Ranid Glowergold")
+	core:addMonsterName("Tiffany Cartier")
+	core:addMonsterName("Bryan Landers")
+	core:addMonsterName("Larana Drome")
+	core:addMonsterName("Patricia Egan")
+	core:addMonsterName("Dominique Stefano")
+	core:addMonsterName("Edward Egan")
+	core:addMonsterName("Dorian Fines")
+	core:addMonsterName("Marcia Chase")
+	core:addMonsterName("Elizabeth Ross")
+	core:addMonsterName("Breanni")
+	core:addMonsterName("Aemara")
+	core:addMonsterName("Anthony Durain")
+	core:addMonsterName("Valaden Silverblade")
+	core:addMonsterName("Valerie Langrom")
+	core:addMonsterName("Aerith Primrose")
+	core:addMonsterName("Abra Cadabra")
+	core:addMonsterName("Paldesse")
+	core:addMonsterName("Griselda Hunderland")
+	core:addMonsterName("Arille Azuregaze")
+	core:addMonsterName("Windle Sparkshine")
+	core:addMonsterName("Dagna Flintlock")
+	core:addMonsterName("Jepetto Joybuzz")
+	core:addMonsterName("Karandonna")
+	core:addMonsterName("Jarold Puller")
+	core:addMonsterName("Shen Kang Cheng")
+	core:addMonsterName("Norvin Alderman")
+	core:addMonsterName("Kerta the Bold")
+	core:addMonsterName("Walther Whiteford")
+	core:addMonsterName("Bartram Haller")
+	core:addMonsterName("Linna Bruder")
+	core:addMonsterName("Lalla Brightweave")
+	core:addMonsterName("Ainderu Summerleaf")
+	core:addMonsterName("Bragund Brightlink")
+	core:addMonsterName("Orton Bennet")
+	core:addMonsterName("Debbi Moore")
+	core:addMonsterName("Ninsianna")
+	core:addMonsterName("Ajay Green")
+	core:addMonsterName("Alchemist Cinesra")
+	core:addMonsterName("Darahir")
+	core:addMonsterName("Hexil Garrot")
+	core:addMonsterName("Applebough")
+	core:addMonsterName("Aimee")
+	core:addMonsterName("Angelique Butler")
+	core:addMonsterName("Hagatha Moorehead")
+	core:addMonsterName("Chameli Banaphash")
+	core:addMonsterName("Sheddle Glossgleam")
+	core:addMonsterName("Lucian Trias")
+	core:addMonsterName("Fialla Sweetberry")
+	core:addMonsterName("Clockwork Assistant")
+	core:addMonsterName("Blazik Fireclaw")
+	core:addMonsterName("Misensi")
+	core:addMonsterName("Derek Odds")
+	core:addMonsterName("Hira Snowdawn")
+	core:addMonsterName("Uda the Beast")
+	core:addMonsterName("Arcanist Adurin")
+	core:addMonsterName("Arcanist Ivrenne")
+	core:addMonsterName("Magister Brasael")
+	core:addMonsterName("Magistrix Lambriesse")
+	core:addMonsterName("Harold Winston")
+	core:addMonsterName("Mei Francis")
+	core:addMonsterName("Kyunghee")
+	core:addMonsterName("Archmage Alvareaux")
+	core:addMonsterName("Nixi Fireclaw")
+	core:addMonsterName("Christi Stockton")
+	core:addMonsterName("Sandra Bartan")
+	core:addMonsterName("Mato")
+	core:addMonsterName("Isirami Fairwind")
+	core:addMonsterName("Hamaka")
+	core:addMonsterName("Stefen Cotter")
+	core:addMonsterName("Umbiwa")
+	core:addMonsterName("Mimbihi")
+	core:addMonsterName("Marcella Bloom")
+	core:addMonsterName("Laire Brewgold")
+	core:addMonsterName("Coira Longrifle")
+	core:addMonsterName("Brammold Deepmine")
+	core:addMonsterName("Vanessa Sellers")
+	core:addMonsterName("Braeg Stoutbeard")
+	core:addMonsterName("Alfred Copperworth")
+	core:addMonsterName("Sarah Brady")
+	core:addMonsterName("Jessica Sellers")
+	core:addMonsterName("Nargle Lashcord")
+	core:addMonsterName("Xazi Smolderpipe")
+	core:addMonsterName("Zom Bocom")
+	core:addMonsterName("Magister Sarien")
+	core:addMonsterName("Arcanist Firael")
+	core:addMonsterName("Trapjaw Rix")
+	core:addMonsterName("Dubin Clay")
+	core:addMonsterName("Arcanist Miluria")
+	core:addMonsterName("Magistrix Vesara")
+	core:addMonsterName("Rueben Lauren")
+	core:addMonsterName("Rafael Langrom")
+	core:addMonsterName("Horace Hunderland")
+	core:addMonsterName("Matilda Brightlink")
+	core:addMonsterName("Enchanter Isian")
+	core:addMonsterName("Enchanter Erodin")
+	core:addMonsterName("Usuri Brightcoin")
+	core:addMonsterName("Kaye Toogie")
+	core:addMonsterName("Magister Arlan")
+	core:addMonsterName("Arcanist Uovril")
+	core:addMonsterName("Frozo the Renowned")
+
+	
+	-- Shattrath
+	core:addMonsterName("Cro Threadstrong")
 end
 
 
